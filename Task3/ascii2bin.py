8b473a3f14ded6d933adaed71849148f7f95f17a
Santiago Narvaez <santiago.narvaez@tum.de>
2018-01-26 15:12:19 +0100

    Check if str is float

--- Task3/ascii2bin.py
+++ Task3/ascii2bin.py
@@ -4,6 +4,14 @@ import argparse
 import struct
 
 
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
 def main():
     parser = argparse.ArgumentParser(description="Converts the input matrix from ASCII to binary")
     parser.add_argument("-i", "--input", help="Input ascii matrix file")
@@ -13,7 +21,7 @@ def main():
     with open(args.output, "wb") as bin_file:
         with open(args.input, "r") as ascii_file:
             for line in ascii_file:
-                numbers = [float(n) for n in line.replace("\n", "").split("\t") if n.isdecimal()]
+                numbers = [float(n) for n in line.replace("\n", "").split("\t") if isfloat(n)]
                 for n in numbers:
                     bin_file.write(struct.pack('d', n))
 

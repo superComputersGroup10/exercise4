9ac88ee72c2b2ea30501ea6bfbdb0ec81366cd5c
Santiago Narvaez <santiago.narvaez@tum.de>
2018-01-28 09:16:49 +0100

    Write header as integer
    The header of the file must be an integer instead of a double

--- Task3/ascii2bin.py
+++ Task3/ascii2bin.py
@@ -18,12 +18,19 @@ def main():
     parser.add_argument("-o", "--output", help="Output binary matrix file")
     args = parser.parse_args()
 
+    is_first_line = True
     with open(args.output, "wb") as bin_file:
         with open(args.input, "r") as ascii_file:
             for line in ascii_file:
-                numbers = [float(n) for n in line.replace("\n", "").split("\t") if isfloat(n)]
-                for n in numbers:
-                    bin_file.write(struct.pack('d', n))
+                if is_first_line:
+                    is_first_line = False
+                    numbers = [int(n) for n in line.replace("\n", "").split("\t") if n.isdecimal()]
+                    for n in numbers:
+                        bin_file.write(struct.pack('i', n))
+                else:
+                    numbers = [float(n) for n in line.replace("\n", "").split("\t") if isfloat(n)]
+                    for n in numbers:
+                        bin_file.write(struct.pack('d', n))
 
 
 
